014ff492835ffaf5aa09579ede21162a9b085b71
binary-shadow <web.dev.innovation.ru@gmail.com>
2025-12-22 22:24:36 +0300

    feat: optimize lottie file

--- vite.config.ts
+++ vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     allowedHosts: [
       'nymb-interface.vercel.app',
-      'air-insurance-qualities-noon.trycloudflare.com',
+      'imposed-supplements-estimated-brad.trycloudflare.com',
     ],
   },
 
