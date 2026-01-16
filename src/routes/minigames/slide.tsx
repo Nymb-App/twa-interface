e7df9e2dd4a2ac59780827c88e5c7bf04e539ef8
Nice Arti <nice_arti@mail.ru>
2026-01-16 14:17:21 +0200

    feat: update config

--- src/routes/minigames/slide.tsx
+++ src/routes/minigames/slide.tsx
@@ -376,11 +376,11 @@ function GameFinished({
           <Link
             to="/"
             className="w-full"
-            onClick={() =>
+            // onClick={() =>
               // setTimeout(() => {
               //   window.location.reload()
               // }, 2000)
-            }
+            // }
           >
             <ActionButton className="text-black bg-gradient-to-b from-white to-[#999999] active:from-[#999999] active:to-[#535353] disabled:from-[#999999] disabled:to-[#535353] disabled:cursor-not-allowed opacity-0 animate-slide-up-fade-swipe-game-6">
               CLOSE
